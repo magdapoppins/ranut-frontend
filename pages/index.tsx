c146619248e22f6da2f0ea53239141266fdc0873
magdapoppins <stenius.mimmy@gmail.com>
2022-05-15 12:56:49 +0300

    Add ranus

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const RanuRain: React.FC<{imageName: string}> = ({imageName}) => <div className={styles.rain} style={{left: `${Math.random()*90}%`, animationDuration: `${Math.random()*40}s`}}>
+  <Image src={imageName} alt="French fry" layout='fill' objectFit='contain' />
+</div>
+
+const ranus = [
+  '/ranu-basic.png',
+  '/ranu-bataatti.png',
+  '/ranu-haitari.png',
+  '/ranu-lytty.png',
+  '/ranu-pitka.png',
+]
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      { ranus.map(imageName => <RanuRain key={imageName} imageName={imageName}/>) }
       <Head>
         <title>Ranut</title>
         <meta name="description" content="Find the best ranus around" />
@@ -23,7 +36,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        Build by <a href="https://github.com/lgrigoryeva1">mila</a> & <a href="https://github.com/magdapoppins">magda</a>
+        <span>Built by <a href="https://github.com/lgrigoryeva1">mila</a> & <a href="https://github.com/magdapoppins">magda</a> & viia</span>
       </footer>
     </div>
   )
